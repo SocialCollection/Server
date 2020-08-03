51042019589071faaabf6c51f87f2feb543d263c
johanneslosch <hi@johanneslosch.com>
2020-08-03 20:31:20 +0200

    github

--- src/main/java/org/socialcollection/connectors/github/Github.java
+++ src/main/java/org/socialcollection/connectors/github/Github.java
@@ -1,56 +1,57 @@
 package org.socialcollection.connectors.github;
 
+import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GitHub;
 import org.socialcollection.util.Config;
-import org.socialcollection.util.Logger;
-import org.socialcollection.util.WriteFile;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Github {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try {
+            System.out.println(getLicense("MIT"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getIssue(String repo, int issue) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+        if (gh.getRepository(repo).hasIssues()) {
+            if(!gh.getRepository(repo).getIssues(GHIssueState.OPEN).get(issue).isPullRequest()){
+                return "ISSUE: " + gh.getRepository(repo).getIssue(gh.getRepository(repo).getIssues(GHIssueState.OPEN).get(issue).getNumber()).getUrl().toString();
+            }else{
+                return "PR: " + gh.getRepository(repo).getIssue(gh.getRepository(repo).getIssues(GHIssueState.OPEN).get(issue).getNumber()).getUrl().toString();
+            }
+        }
+        return null;
+    }
+    public static URL getUserURL(String user) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+        return gh.getUser(user).getHtmlUrl();
+    }
+    public static String getLicense(String licenseName) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+        return getLicenseName(licenseName) + " \n" + getLicenseBody(licenseName) + "\n URL: " + getLicenseURL(licenseName);
+    }
+    private static String getLicenseBody(String licenseName) throws IOException {
         GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
 
-        //returns coding lang
-        //System.out.println(gh.getMyself().getAllRepositories().get("personalWebsite").getLanguage());
-        //returns MIT LICENSE
-        //System.out.println(gh.getMyself().getAllRepositories().get("re.call").getLicense().getName());
-        //returns johanneslosch/re.call
-        //System.out.println(gh.getMyself().getAllRepositories().get("re.call").getFullName());
-        //creates repo, output
-        //gh.createRepository("test").description("test description").issues(true).private_(false).create();
-        //gh.getMyself().getAllRepositories().get("test").createIssue("first issue").body("body message").label("bug").assignee(gh.getUser("johanneslosch")).create();
-        //gh.getMyself().getAllRepositories().get("test").setPrivate(false);;
-        //System.out.println(gh.getRepository("johanneslosch/re.call").getLanguage());
-
-        if(!gh.isCredentialValid()){
-            System.exit(1);
-            Logger.error("Invalid credentials");
-        }
-        if(gh.isOffline()){
-            System.out.println("offline");
-        }
-        String repo = "firstcontributions/first-contributions";
-        if(gh.getRepository(repo).hasIssues()){
-            System.out.println(
-            gh.getRepository(repo).getIssue(26602).getBody());
-        }
-        //System.out.println(gh.getRepository("firstcontributions/first-contributions").hasIssues());
-        //System.out.println(gh.getRepository("firstcontributions/first-contributions").getForksCount());
-        //System.out.println(Arrays.toString(gh.getUser("singhashish18").getKeys().toArray()));
-        //followers(gh);
-        //System.out.println(gh.getRepository("johanneslosch/re.call").getCompare("dev", "master").getDiffUrl().toString());
-    }
-    public String getLicenseBody(GitHub gh, String licenseName) throws IOException {
         return gh.getLicense(licenseName).getBody();
     }
-    public static void followers(GitHub gh) throws IOException {
-        writeFile(gh.getMyself().getFollowers().byLogin("singhashish18").getFollowers().toString());
+    private static String getLicenseURL(String licenseName) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+
+        return String.valueOf(gh.getLicense(licenseName).getUrl());
+    }
+    private static String getLicenseName(String licenseName) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+
+        return gh.getLicense(licenseName).getName();
+    }
+    public static String getFollowers(String user) throws IOException {
+        GitHub gh = GitHub.connect(Config.readConfig("data", "secrets", "GHlogin"), Config.readConfig("data", "secrets", "GHoauth"));
+        return gh.getMyself().getFollowers().byLogin(user).getFollowers().toString();
 
     }
-    public static void writeFile(String content){
-        WriteFile.writer("data", "followers", "txt", content);
-    }
-
 }
